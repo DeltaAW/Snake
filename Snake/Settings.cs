a1384f4307ad92e03246e57153cea91aff5e11d6
DeltaAW <Ar.vagner@gmail.com>
2025-09-29 17:44:45 +0200

    Update Settings.cs

--- Snake/Settings.cs
+++ Snake/Settings.cs
@@ -10,62 +10,59 @@
             {
                 Console.Clear();
 
-                Console.WriteLine("Выберите размер игрового поля\n");
+                Console.WriteLine("Wählen Sie die Größe des Spielfeldes\n");
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("\t1 - маленькое\n");
-                Console.WriteLine("\t2 - среднее\n");
-                Console.WriteLine("\t3 - большое\n");
+                Console.WriteLine("\t1 - klein\n");
+                Console.WriteLine("\t2 - mittel\n");
+                Console.WriteLine("\t3 - groß\n");
                 Console.ResetColor();
 
-
-                Console.WriteLine("Выберите сложность\n");
+                Console.WriteLine("Wählen Sie den Schwierigkeitsgrad\n");
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("\t4 - легкий\n");
+                Console.WriteLine("\t4 - einfach\n");
                 Console.ResetColor();
-                Console.WriteLine("\t5 - средний\n");
+                Console.WriteLine("\t5 - mittel\n");
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\t6 - сложный\n");
-
-
+                Console.WriteLine("\t6 - schwer\n");
                 Console.ResetColor();
 
-                Console.Write("размер игрового поля: ");
+                Console.Write("Spielfeldgröße: ");
                 Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine(fliedSize);
                 Console.ResetColor();
 
-                Console.Write("сложность игры ");
+                Console.Write("Schwierigkeitsgrad: ");
                 Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine(complexity);
                 Console.ResetColor();
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("\n\t7 - назад\n");
+                Console.WriteLine("\n\t7 - zurück\n");
                 Console.ResetColor();
 
                 if (int.TryParse(Console.ReadLine(), out number) && number == 1)
                 {
-                    fliedSize = "small";
+                    fliedSize = "klein";
                 }
                 else if (number == 2)
                 {
-                    fliedSize = "average";
+                    fliedSize = "mittel";
                 }
                 else if (number == 3)
                 {
-                    fliedSize = "big";
+                    fliedSize = "groß";
                 }
                 else if (number == 4)
                 {
-                    complexity = "easy";
+                    complexity = "einfach";
                 }
                 else if (number == 5)
                 {
-                    complexity = "average";
+                    complexity = "mittel";
                 }
                 else if (number == 6)
                 {
-                    complexity = "difficult";
+                    complexity = "schwer";
                 }
                 else if (number == 7)
                 {
@@ -74,9 +71,9 @@
                 else
                 {
                     Console.Clear();
-                    Console.WriteLine("Вы ввели не верное значение, попробуйте еще раз");
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Нажмите ENTER, чтобы продолжить");
+                    Console.WriteLine("Ungültige Eingabe, bitte erneut versuchen.");
+                    Console.WriteLine("Drücken Sie ENTER, um fortzufahren");
                     Console.ResetColor();
                     Console.ReadLine();
                     continue;
@@ -85,3 +82,4 @@
         }
     }
 }
+
